e97629cbdf69fc3ee849e8e535551395d1668c0f
Nathania <96285003+nathaniafernandes@users.noreply.github.com>
2022-05-21 18:16:13 +0530

    Add files via upload

---
+++ KMeans_Clustering.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # NATHANIA FERNANDES
+# 
+# 
+# ### 
+# ## K- Means Clustering
+# 
+
+# In[57]:
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+
+# Load the iris dataset
+iris = pd.read_csv('C:\\Users\Melywn Fernades\Downloads\Iris.csv')
+iris_df = pd.DataFrame(iris, columns=['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm'])
+iris_df
+
+
+# In[56]:
+
+
+x = iris_df.iloc[:, [0, 1, 2, 3]].values
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+# Plotting the results to a line graph, allowing us to observe 'The elbow'
+plt.plot(range(1, 11), wcss, c = 'darkred')
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS') # Within cluster sum of squares
+plt.show()
+
+
+# In[41]:
+
+
+# Applying kmeans to the dataset / Creating the kmeans classifier
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+
+# In[47]:
+
+
+# Visualising the clusters - On the first two columns
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'orangered', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'darkred', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'lightsalmon', label = 'Iris-virginica')
+
+# Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
